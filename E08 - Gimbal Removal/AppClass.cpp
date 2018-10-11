2807ba02c354d07382aa6c9a72de837bf1d09af8
MAIN\blt7138 <blt7138@g.rit.edu>
2018-10-10 20:35:20 -0400

    E08

--- E08 - Gimbal Removal/AppClass.cpp
+++ E08 - Gimbal Removal/AppClass.cpp
@@ -1,4 +1,7 @@
 #include "AppClass.h"
+
+vector3 prevRot = vector3(0.0f);
+
 void Application::InitVariables(void)
 {
 	//init the mesh
@@ -21,17 +24,37 @@ void Application::Display(void)
 {
 	// Clear the screen
 	ClearScreen();
-
+	
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
-
-	m_m4Model = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
+	 
+	/*m_m4Model = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
 	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
-	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
+	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));*/
 
-	//m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), vector3(1.0f));
-	//m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
+
+	if (m_v3Rotation == vector3(0.0f)) //if reset
+	{
+		prevRot = vector3(0.0f);
+		m_qOrientation = IDENTITY_QUAT;
+	}
+	if (m_v3Rotation != prevRot) //the pyramid has been rotated
+	{
+		if (m_v3Rotation.x != prevRot.x)
+		{
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), vector3(m_v3Rotation.x - prevRot.x, 0.0f, 0.0f));
+		}
+		if (m_v3Rotation.y != prevRot.y)
+		{
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), vector3(0.0f, m_v3Rotation.y - prevRot.y, 0.0f));
+		}
+		if (m_v3Rotation.z != prevRot.z)
+		{
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), vector3(0.0f, 0.0f, m_v3Rotation.z - prevRot.z));
+		}
+		prevRot = m_v3Rotation;
+	}
+	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
 	
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
